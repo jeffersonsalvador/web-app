0b1af550a8344276c8c4b70a43da271b39e0626d
Jefferson Costa <jefferson.costa@pdmfc.com>
2019-11-08 14:55:54 +0000

    Webpack changed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: path.resolve('dist'),
+        path: path.resolve('public/assets/js/'),
         filename: 'main.js'
     }
 }
